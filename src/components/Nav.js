0808953506db5e215da9bda32779fe3fd5294556
Murphy-ZJ <murphy.zachary@outlook.com>
2021-08-14 17:55:47 -0400

    final commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,14 @@ function Nav() {
         <div className="navbar-start">
 
             <NavLink to='/' className="navbar-item">Home</NavLink>
+            <NavLink to='/NeoVim' className="navbar-item">Neovim</NavLink>
+            <NavLink to='/Bat' className="navbar-item">Bat</NavLink>
+            <NavLink to='/Lsd' className="navbar-item">LSD</NavLink>
+            <NavLink to='/Ripgrep' className="navbar-item">ripgrep</NavLink>
+            <NavLink to='/Kitty' className="navbar-item">Kitty Terminal Emulator</NavLink>
+            <NavLink to='/Fish' className="navbar-item">Fish Shell</NavLink>
+            <NavLink to='/VimPlug' className="navbar-item">Vim Plug</NavLink>
+            <NavLink to='/CocNvim' className="navbar-item">coc.nvim</NavLink>
 
         </div>
 
